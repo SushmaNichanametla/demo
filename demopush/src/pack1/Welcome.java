f41d0f9ed116537bec9be4e6234cc25458fdac01
SushmaNichanametla <219x1a0568@gprec.ac.in>
2023-10-30 11:57:16 +0530

    second commit

--- demopush/src/pack1/Welcome.java
+++ demopush/src/pack1/Welcome.java
@@ -3,7 +3,8 @@ package pack1;
 public class Welcome {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to GPREC");		// TODO Auto-generated method stub
+		System.out.println("Welcome to GPREC");	
+		System.out.println("My name is sushma");// TODO Auto-generated method stub
 
 	}
 
